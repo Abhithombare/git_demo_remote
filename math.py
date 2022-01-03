ffdc9bc60180db484f4f936cd771aa71c28446ac
Abhithombare <55059098+Abhithombare@users.noreply.github.com>
2022-01-03 10:24:50 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+#additon fuction
+def add(x,y):
+    return x+y
+#subtraction function
+def sub(x,y):
+    return x-y       #master branch
+#multiplication function
+def mul(x,y):
+    return x*y
+#division function
+def div(x,y):
+    if y==0:
+        return("Null")  #master branch
+    else:
+        return x/y
+#square function
+def square(x):
+    return x*x
